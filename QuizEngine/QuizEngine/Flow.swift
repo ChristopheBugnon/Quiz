398e1b719dbce59e371c31eaf45e2c094e6eff48
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2021-10-18 19:52:19 +0200

    Start with two questions and answer first question routes to second question

--- QuizEngine/QuizEngine/Flow.swift
+++ QuizEngine/QuizEngine/Flow.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol Router {
-    func routeTo(question: String)
+    func routeTo(question: String, answerCallback: @escaping (String) -> Void)
 }
 
 class Flow {
@@ -22,7 +22,12 @@ class Flow {
 
     func start() {
         if let firstQuestion = questions.first {
-            router.routeTo(question: firstQuestion)
+            router.routeTo(question: firstQuestion) { [weak self] _ in
+                guard let self = self else { return }
+                let firstQuestionIndex = self.questions.firstIndex(of: firstQuestion)!
+                let nextQuestion = self.questions[firstQuestionIndex+1]
+                self.router.routeTo(question: nextQuestion) { _ in }
+            }
         }
     }
 }
