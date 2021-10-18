0a79c9056a19bca511457e1952b48d3e72b99d42
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2021-10-18 22:34:52 +0200

    QuestionVC renders zero options with no options on view did load

--- QuizApp/QuizAppTests/QuestionViewControllerTest.swift
+++ QuizApp/QuizAppTests/QuestionViewControllerTest.swift
@@ -12,10 +12,18 @@ import XCTest
 class QuestionViewControllerTest: XCTestCase {
 
     func test_viewDidLoad_rendersQuestionHeaderText() {
-        let sut = QuestionViewController(question: "Q1")
+        let sut = QuestionViewController(question: "Q1", options: [])
 
         _ = sut.view
 
         XCTAssertEqual(sut.headerLabel.text, "Q1")
+    }
+
+    func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
+        let sut = QuestionViewController(question: "", options: [])
+
+        _ = sut.view
+
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
     }
 }
