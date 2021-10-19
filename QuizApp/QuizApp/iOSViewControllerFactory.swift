ad0dbf928983d32fbc7932bbf76a0988ad480134
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2021-10-19 21:58:03 +0200

    Refactor production code

--- QuizApp/QuizApp/iOSViewControllerFactory.swift
+++ QuizApp/QuizApp/iOSViewControllerFactory.swift
@@ -16,11 +16,19 @@ class iOSViewControllerFactory: ViewControllerFactory {
     }
 
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
+        guard let options = options[question] else {
+            fatalError("Couldn't find options for question \(question)")
+        }
+
+        return questionViewController(for: question, options: options, answerCallback: answerCallback)
+    }
+
+    private func questionViewController(for question: Question<String>, options: [String], answerCallback: @escaping ([String]) -> Void) -> UIViewController {
         switch question {
         case let .singleAnswer(value):
-            return QuestionViewController(question: value, options: options[question]!, selection: answerCallback)
+            return QuestionViewController(question: value, options: options, selection: answerCallback)
         case let .multipleAnswer(value):
-            let controller = QuestionViewController(question: value, options: options[question]!, selection: answerCallback)
+            let controller = QuestionViewController(question: value, options: options, selection: answerCallback)
             _ = controller.view
             controller.tableView.allowsMultipleSelection = true
             return controller
