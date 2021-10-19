6e85bc983f3f71c3aba8084ca62c3fe763399a8f
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2021-10-19 21:26:12 +0200

    Create share scope class variable to avoid duplication

--- QuizApp/QuizAppTests/iOSViewControllerFactoryTest.swift
+++ QuizApp/QuizAppTests/iOSViewControllerFactoryTest.swift
@@ -10,22 +10,25 @@ import XCTest
 @testable import QuizApp
 
 class iOSViewControllerFactoryTest: XCTestCase {
-    func test_questionViewController_createsControllerWithQuestion() {
-        let sut = iOSViewControllerFactory(options: [Question.singleAnswer("Q1"): ["A1"]])
+    private let questionSingleAnswer = Question.singleAnswer("Q1")
 
-        let controller = sut.questionViewController(for: Question.singleAnswer("Q1"), answerCallback: { _ in }) as? QuestionViewController
+    func test_questionViewController_createsControllerWithQuestion() {
+        let sut = iOSViewControllerFactory(options: [questionSingleAnswer: ["A1"]])
+
+        let controller = sut.questionViewController(for: questionSingleAnswer, answerCallback: { _ in }) as? QuestionViewController
 
         XCTAssertNotNil(controller)
         XCTAssertEqual(controller?.question, "Q1")
     }
 
     func test_questionViewController_createsControllerWithOptions() {
-        let sut = iOSViewControllerFactory(options: [Question.singleAnswer("Q1"): ["A1", "A2"]])
+        let options = ["A1", "A2"]
+        let sut = iOSViewControllerFactory(options: [questionSingleAnswer: options])
 
-        let controller = sut.questionViewController(for: Question.singleAnswer("Q1"), answerCallback: { _ in }) as? QuestionViewController
+        let controller = sut.questionViewController(for: questionSingleAnswer, answerCallback: { _ in }) as? QuestionViewController
 
         XCTAssertNotNil(controller)
-        XCTAssertEqual(controller?.options, ["A1", "A2"])
+        XCTAssertEqual(controller?.options, options)
     }
 
 }
